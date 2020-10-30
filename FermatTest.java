aaca5db9f9f96aa11886fd81198b0ce72eadf964
Jake <jake.peterson7@aol.com>
2020-10-30 17:15:03 -0500

    Implement checkComposite.

--- FermatTest.java
+++ FermatTest.java
@@ -16,7 +16,8 @@ public class FermatTest{
     public static void printBigInt(BigInteger x){
         System.out.println(x.toString());
     }
-    
+    // This primality test will check if n is prime or not.
+    // Will return true if prime.
     public static boolean testPrimality(BigInteger n, int s){
         boolean prime = true;
         int counter = 0;  //counter for numbers that we've already tried.
@@ -31,10 +32,16 @@ public class FermatTest{
         return prime;
     }
 
-    private static boolean checkComposite(BigInteger a, BigInteger p){
-        return true;
+    private static boolean checkComposite(BigInteger a, BigInteger n){
+        boolean composite = true;
+        BigInteger one = new BigInteger("1");
+        BigInteger diff = n.subtract(one);
+        if(a.modPow(diff, n).equals(1) ){
+            composite = false;
+        }
+        return composite;
     }
-
+    // Stub
     private static BigInteger pickRandomInRange(BigInteger p) {
         return new BigInteger(512, new Random());
     }
