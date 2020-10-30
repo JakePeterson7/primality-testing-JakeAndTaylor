adf77d633d584661862ad21d6aa6243d2a9eba69
Jake <jake.peterson7@aol.com>
2020-10-30 16:30:50 -0500

    Setup primality test
    methods that are used by the test still need to be implemented and are currently stubs.

--- FermatTest.java
+++ FermatTest.java
@@ -3,7 +3,6 @@ import java.util.Random;
 
 public class FermatTest{
     public static void main(String[] args){
-        int a = 0;  //counter for numbers that we've already tried.
         int s = 10; // security parameter
         // create random BigInteger of 512 bits
         Random rnd = new Random();
@@ -11,11 +10,33 @@ public class FermatTest{
         BigInteger p = new BigInteger(numBits, rnd); // random BigInteger of bit length numBits
         BigInteger prime = BigInteger.probablePrime(numBits, rnd); // random prime of bit length numBits
         
-        System.out.println(p.toString());
         printBigInt(p);
 
     }
     public static void printBigInt(BigInteger x){
         System.out.println(x.toString());
     }
+    
+    public static boolean testPrimality(BigInteger n, int s){
+        boolean prime = true;
+        int counter = 0;  //counter for numbers that we've already tried.
+
+        while(counter <= s){
+            BigInteger a = pickRandomInRange(n);
+            if(checkComposite(a, n)){
+                prime = false;
+            }
+            s++;
+        }
+        return prime;
+    }
+
+    private static boolean checkComposite(BigInteger a, BigInteger p){
+        return true;
+    }
+
+    private static BigInteger pickRandomInRange(BigInteger p) {
+        return new BigInteger(512, new Random());
+    }
+
 }
