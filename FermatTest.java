00b2a0f5835e0db1a7d01015c2037008092eeb45
Jake <jake.peterson7@aol.com>
2020-10-30 18:06:19 -0500

    More tests

--- FermatTest.java
+++ FermatTest.java
@@ -7,15 +7,28 @@ public class FermatTest{
         // create random BigInteger of 512 bits
         Random rnd = new Random();
         int numBits = 512;
+
         BigInteger p = new BigInteger(numBits, rnd); // random BigInteger of bit length numBits
         BigInteger prime = BigInteger.probablePrime(numBits, rnd); // random prime of bit length numBits
+        BigInteger ninetySeven = new BigInteger("97");
+        BigInteger ninetyEight = new BigInteger("98");
 
         System.out.println("Big ints to check");
+        //non-primes
+        printBigInt(ninetyEight);
         printBigInt(p);
+        //primes
+        printBigInt(ninetySeven);
         printBigInt(prime);
 
-        System.out.println(testPrimality(p, s));
-        System.out.println(testPrimality(prime, s));
+        System.out.println("Non-primes");
+        //System.out.println(testPrimality(p, s));
+        System.out.println(testPrimality(ninetyEight, s));
+
+        System.out.println("Primes:");
+      //  System.out.println(testPrimality(prime, s));
+        System.out.println(testPrimality(ninetySeven, s));
+
 
     }
     public static void printBigInt(BigInteger x){
@@ -29,6 +42,7 @@ public class FermatTest{
 
         while(counter <= s){
             BigInteger a = pickRandomInRange(n);
+            printBigInt(a);
             if(checkComposite(a, n)){
                 prime = false;
             }
